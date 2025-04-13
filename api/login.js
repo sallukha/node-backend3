1ca92db053a698cacd62a231f0aa61de52d0b3eb
sallu khan <sallukhan54159@gmail.com>
2025-04-13 12:32:59 +0530

    add log

--- api/login.js
+++ api/login.js
@@ -2,6 +2,7 @@ const dbConnect = require('../db/config');
 const User = require('../model/user_modal');
 
 module.exports = async (req, res) => {
+  console.log({req, res})
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Only POST method is allowed' });
   }
@@ -9,11 +10,14 @@ module.exports = async (req, res) => {
   await dbConnect();
   const { email, password } = req.body;
 
+  console.log({email, password})
+
   try {
     const user = await User.findOne({ email, password });
+    consle.log({user})
     if (!user) {
       return res.status(404).json({ message: 'Invalid credentials' });
-    }
+    } d
 
     res.status(200).json({ message: 'Login successful', user });
   } catch (err) {
